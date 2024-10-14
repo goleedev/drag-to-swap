bc6cc34c0264cec5c4c5e0a4f4b2ac911836d578
GO Lee <golee.dev@gmail.com>
2024-10-14 23:23:30 +0100

    feat: add theme.js file with color, spacing, and size constants

---
+++ styles/theme.js
@@ -0,0 +1,17 @@
+export const colors = {
+  primary: '#2778A5',
+  white: '#FFFFFF',
+  text: '#585858',
+};
+
+export const spacing = {
+  md: '16px',
+  lg: '20px',
+};
+
+export const size = {
+  sm: '14px',
+  md: '16px',
+  lg: '20px',
+  full: '100%',
+};
