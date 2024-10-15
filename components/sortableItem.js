0bcd97f8d07c785c40997d70a4922ea1a35a3374
GO Lee <golee.dev@gmail.com>
2024-10-15 22:48:14 +0100

    refactor: refactor SortableItem component to use useDragAndDrop hook

--- components/sortableItem.js
+++ components/sortableItem.js
@@ -1,58 +1,17 @@
-import { useSortable } from '@dnd-kit/sortable';
 import Image from 'next/image';
-import { useCallback, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
+import { useDragAndDrop } from '../hooks/useDragAndDrop';
 import { DragPreview, PrintPhoto } from '../styles/sortableItemStyles';
 import ImageIcon from './icons/ImageIcon';
 
 const SortableItem = ({ id, url }) => {
-  const { attributes, listeners, setNodeRef, isDragging } = useSortable({ id });
-
-  const [showPreview, setShowPreview] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [withinBounds, setWithinBounds] = useState(false);
-
-  const updatePosition = useCallback(
-    (e) => {
-      if (isDragging) {
-        const x = e.clientX;
-        const y = e.clientY;
-
-        const isWithinBounds =
-          x > 0 && y > 0 && x < window.innerWidth && y < window.innerHeight;
-
-        setWithinBounds(isWithinBounds);
-        setPosition({ x, y });
-      }
-    },
-    [isDragging]
-  );
-
-  useEffect(() => {
-    window.addEventListener('mousemove', updatePosition);
-    window.addEventListener('touchmove', (e) => updatePosition(e.touches[0]));
-
-    return () => {
-      window.removeEventListener('mousemove', updatePosition);
-      window.removeEventListener('touchmove', (e) =>
-        updatePosition(e.touches[0])
-      );
-    };
-  }, [updatePosition]);
-
-  useEffect(() => {
-    setShowPreview(isDragging && withinBounds);
-  }, [isDragging, withinBounds]);
+  const { attributes, listeners, setNodeRef, showPreview, position, style } =
+    useDragAndDrop(id);
 
   return (
     <>
-      <PrintPhoto
-        ref={setNodeRef}
-        style={{ opacity: isDragging ? 0.5 : 1 }}
-        {...attributes}
-        {...listeners}
-      >
+      <PrintPhoto ref={setNodeRef} style={style} {...attributes} {...listeners}>
         {url ? (
           <Image
             width={600}
